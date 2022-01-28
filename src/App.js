9bba810ed39c34c897b8c40c5bce35d45c5d2c3c
Jorge Diaz <jorgeddc@gmail.com>
2022-01-28 09:49:45 +0100

    cronometro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+let run
+let sec = 1;
+let min = 1;
+let hor = 1;
+const Start = () =>{
+    document.querySelector('.startButton').disabled = true;
+    run = setInterval( 
+        function runStart()
+        {
+            const second = document.querySelector('.second')
+            const minute = document.querySelector('.minute')
+            const hour = document.querySelector('.hour')
+            if ( sec<=9){second.innerHTML = `0${sec++}`} 
+            else{second.innerHTML = sec++}
+            
+            if (sec>=60){
+                second.innertHTML= '00' 
+                 sec=0;
+                 
+                 
+                
+                 if ( min<=9){minute.innerHTML = `0${min++}`} 
+                 else{minute.innerHTML = min++}
+                 return
+                
+         }
+         if (min>=60){
+            minute.innertHTML= '00' 
+             min=0;
+           
+             if ( hor<=9){hour.innerHTML = `0${hor++}`} 
+             else{hour.innerHTML = hor++}
+             return
+            
+     }
+            
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            },1000);
+        }
+        const Clear = () =>{
+            
+            clearInterval(run)
+         document.querySelector('.startButton').disabled = false;
+           
+            document.querySelector('.second').innerHTML='00';
+            sec=0;
+            document.querySelector('.minute').innerHTML='00';
+            min=0;
+            document.querySelector('.hour').innerHTML='00';
+            hor=0;
+
+
+
+        
+        }
+        
+        const Stop = () =>{
+         document.querySelector('.startButton').disabled = false;
+
+           clearInterval(run)
+           console.log('para ', sec)
+        }
+
+        // const reinicio = () =>
+        // {
+        //     second.innertHTML= '00' 
+        // }
+    
+const App = () =>{
+
+   
+    return (
+        <> 
+            <div className='contenedor-crono'>
+            <h1 className='hour'>00 </h1> :<h1 className='minute'> 00</h1>  :<h1 className='second'>00</h1> </div>
+            <div className="contenedor-button">  <button className='startButton'onClick = {Start}>start</button>
+            <button onClick={Clear}>clear</button>
+            <button  onClick = {Stop}>stop</button>
+            </div>
+        </>
+      
+        )
+   
 }
+
+
 
 export default App;
